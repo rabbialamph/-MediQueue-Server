1a87c7646af134c9a7c2106c4190b7c526bd9969
rabbialamph <mdrabbialom98@gmail.com>
2026-05-21 12:31:01 +0600

    tutors api add

--- index.js
+++ index.js
@@ -31,8 +31,26 @@ async function run() {
     const tutorsCollection = db.collection("tutors");
     const bookingCollection = db.collection("booking");
 
+    app.post("/tutors", verifyToken, async (req, res) => {
+      try {
+        const tutor = req.body;
 
-    
+        const tutorData = {
+          ...tutor,
+          totalSlot: Number(tutor.totalSlot),
+          hourlyFee: Number(tutor.hourlyFee),
+        };
+
+        const result = await tutorsCollection.insertOne(tutorData);
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: error.message });
+      }
+    });
+
+
+
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ Server error:", error);
